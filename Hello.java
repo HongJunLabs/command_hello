212b6bfb952a940d419e8d24b053fa5850af0909
HongJunLabs <42328518+HongJunLabs@users.noreply.github.com>
2018-08-13 17:16:06 +0900

    added output "Hello her World"

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello
    public static void main(String[] args)throws Exception{
    	System.out.println("Hello World");
    	System.out.println("Tell Your World");
+   	System.out.println("Tell her World");
    }
 }
